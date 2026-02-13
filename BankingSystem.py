a006b0cda6d04926f3e445b21aa4655a600f525f
Pablito248 <ajuanpa995@gmail.com>
2026-02-12 21:15:29 -0500

    Se implemento la funcionalidad de depositar dinero

---
+++ BankingSystem.py
@@ -0,0 +1,14 @@
+class BankingSystem:
+    def __init__(self, saldo_inicial=0):
+        self.saldo = saldo_inicial
+
+    def depositar(self, monto):
+        if monto <= 0:
+            return 'El monto debe ser mayor que 0.'
+
+
+        self.saldo += monto
+        return f'Depósito exitoso. Nuevo saldo ${self.saldo}'
+
+    def obtener_saldo(self):
+        return self.saldo    
---
+++ CheckBalance.py
@@ -0,0 +1,25 @@
+from BankingSystem import BankingSystem
+
+cuenta = BankingSystem()
+
+while True:
+    print("\n1. Depositar")
+    print("2. Consultar saldo")
+    print("3. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        monto = float(input("Ingrese el monto a depositar: "))
+        print(cuenta.depositar(monto))
+
+    elif opcion == "2":
+        print("Saldo actual:", cuenta.consultar_saldo())
+
+    elif opcion == "3":
+        print("Saliendo...")
+        break
+
+    else:
+        print("Opción inválida.")
+
